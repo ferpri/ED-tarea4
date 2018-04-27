199cce5b964ae1a4a1ed804f65433490cc2c9522
Hara <Hara@Hara-PC.home>
2018-04-27 19:04:16 +0200

    Subida proyecto DAW_ED04_Actividad

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -9,14 +9,14 @@ package dam_ed04_actividad;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1;
+        CCuenta cuenta2;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
+        cuenta2 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta2.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta2);
     }
 
     private static void operativa_cuenta(CCuenta cuenta1) {
